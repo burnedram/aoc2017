e3aa3bd5b39883b5ca57e6779f256d40cb25bf3b
Rafael Mohlin <rafael.mohlin@consid.se>
2019-04-15 17:29:04 +0200

    Rewrite JQuery

--- src/shared/helpers.ts
+++ src/shared/helpers.ts
@@ -2,29 +2,11 @@ interface IJQueryParentNode {
     append(...nodes: Array<string | Node>): void;
 }
 
-class JQueryParentNodeImpl implements IJQueryParentNode {
-    constructor(private el: ParentNode) {}
-
-    public append(...nodes: Array<string | Node>): void {
-        this.el.append(...nodes);
-    }
-}
-
 interface IJQueryNode {
     empty(): void;
 }
 
-class JQueryNodeImpl implements IJQueryNode {
-    constructor(private el: Node) {}
-
-    public empty(): void {
-        while (this.el.firstChild) {
-            this.el.firstChild.remove();
-        }
-    }
-}
-
-interface IJQueryElement {
+interface IJQueryElement extends IJQueryParentNode, IJQueryNode {
     on(
         type: string,
         listener: EventListenerOrEventListenerObject,
@@ -37,76 +19,50 @@ interface IJQueryElement {
     ): void;
 }
 
-class JQueryElementImpl implements IJQueryElement {
-    constructor(private el: Element | Document) {}
-
-    public on(
-        type: string,
-        listener: EventListenerOrEventListenerObject,
-        options?: boolean | AddEventListenerOptions | undefined,
-    ): void {
-        this.el.addEventListener(type, listener, options);
-    }
-
-    public ready(
-        listener: EventListenerOrEventListenerObject,
-        options?: boolean | AddEventListenerOptions,
-    ): void {
-        this.el.addEventListener('DOMContentLoaded', listener, options);
-    }
-}
-
-interface IJQueryHTMLElement {
+interface IJQueryHTMLElement extends IJQueryElement {
     text(val?: string): string;
 }
 
-class JQueryHTMLElementImpl implements IJQueryHTMLElement {
-    constructor(private el: HTMLElement) {}
-
-    public text(val?: string): string {
-        if (val !== undefined) {
-            this.el.innerText = val;
-        }
-        return this.el.innerText;
-    }
-}
-
-interface IJQueryHTMLInputElement {
+interface IJQueryHTMLInputElement extends IJQueryHTMLElement {
     value(val?: string): string;
 }
 
-class JQueryHTMLInputElementImpl implements IJQueryHTMLInputElement {
-    constructor(private el: HTMLInputElement) {}
+type IJQueryHTMLDocument = IJQueryElement;
 
-    public value(val?: string): string {
-        if (val !== undefined) {
-            this.el.value = val;
-        }
-        return this.el.value;
-    }
+interface IJQuery extends IJQueryHTMLElement, IJQueryHTMLInputElement, IJQueryHTMLDocument {
 }
 
-class JQueryHTMLElement implements IJQueryParentNode, IJQueryNode, IJQueryElement, IJQueryHTMLElement {
-    private readonly parentNode: JQueryParentNodeImpl;
-    private readonly node: JQueryNodeImpl;
-    private readonly element: JQueryElementImpl;
-    private readonly htmlElement: JQueryHTMLElementImpl;
-    private readonly htmlInputElement: JQueryHTMLInputElementImpl | null;
+type HTMLTypes = HTMLElement | HTMLInputElement | HTMLDocument;
 
-    constructor(el: HTMLElement) {
-        this.parentNode = new JQueryParentNodeImpl(el);
-        this.node = new JQueryNodeImpl(el);
-        this.element = new JQueryElementImpl(el);
-        this.htmlElement = new JQueryHTMLElementImpl(el);
-        this.htmlInputElement = el instanceof HTMLInputElement ? new JQueryHTMLInputElementImpl(el) : null;
+class JQuery implements IJQuery {
+    private readonly element: HTMLElement;
+    private readonly inputElement: HTMLInputElement;
+    private readonly document: HTMLDocument;
+
+    constructor(el: string | HTMLTypes) {
+        let element: HTMLTypes;
+        if (typeof el === 'string') {
+            const elNull = document.querySelector<HTMLElement>(el);
+            if (elNull === null) {
+                throw new Error(`Could not find element with selector "${el}"`);
+            }
+            element = elNull;
+        } else {
+            element = el;
+        }
+        this.element = element as HTMLElement;
+        this.inputElement = element as HTMLInputElement;
+        this.document = element as HTMLDocument;
     }
 
     public append(...nodes: Array<string | Node>): void {
-        this.parentNode.append(...nodes);
+        this.element.append(...nodes);
     }
 
     public empty(): void {
-        this.node.empty();
+        while(this.element.firstChild) {
+            this.element.firstChild.remove();
+        }
     }
 
     public on(
@@ -114,83 +70,33 @@ class JQueryHTMLElement implements IJQueryParentNode, IJQueryNode, IJQueryElemen
         listener: EventListenerOrEventListenerObject,
         options?: boolean | AddEventListenerOptions | undefined,
     ): void {
-        this.element.on(type, listener, options);
+        this.element.addEventListener(type, listener, options);
     }
 
     public ready(
         listener: EventListenerOrEventListenerObject,
         options?: boolean | AddEventListenerOptions,
     ): void {
-        this.element.ready(listener, options);
+        this.element.addEventListener('DOMContentLoaded', listener, options);
     }
 
     public text(val?: string): string {
-        return this.htmlElement.text(val);
+        if (val !== undefined) {
+            this.element.innerText = val;
+        }
+        return this.element.innerText;
     }
 
-    public value(val?: string | undefined): string {
-        if (this.htmlInputElement === null) {
-            throw new Error('Element is not an input');
+    public value(val?: string): string {
+        if (val !== undefined) {
+            this.inputElement.value = val;
         }
-        return this.htmlInputElement.value(val);
+        return this.inputElement.value;
     }
 }
 
-class JQueryHTMLDocument implements IJQueryParentNode, IJQueryNode, IJQueryElement {
-    private readonly parentNode: JQueryParentNodeImpl;
-    private readonly node: JQueryNodeImpl;
-    private readonly element: JQueryElementImpl;
-
-    constructor(el: HTMLDocument) {
-        this.parentNode = new JQueryParentNodeImpl(el);
-        this.node = new JQueryNodeImpl(el);
-        this.element = new JQueryElementImpl(el);
-    }
-
-    public append(...nodes: Array<string | Node>): void {
-        this.parentNode.append(...nodes);
-    }
-
-    public empty(): void {
-        this.node.empty();
-    }
-
-    public on(
-        type: string,
-        listener: EventListenerOrEventListenerObject,
-        options?: boolean | AddEventListenerOptions | undefined,
-    ): void {
-        this.element.on(type, listener, options);
-    }
-
-    public ready(
-        listener: EventListenerOrEventListenerObject,
-        options?: boolean | AddEventListenerOptions,
-    ): void {
-        this.element.ready(listener, options);
-    }
-}
-
-type JQueryIn = string | HTMLElement | HTMLDocument;
-type JQueryOut = JQueryHTMLElement | JQueryHTMLDocument;
-
-export function $(el: HTMLDocument): JQueryHTMLDocument;
-export function $(el: string | HTMLElement): JQueryHTMLElement;
-export function $(el: JQueryIn): JQueryOut {
-    if (el instanceof HTMLDocument) {
-        return new JQueryHTMLDocument(el);
-    }
-    let element: HTMLElement;
-    if (typeof el === 'string') {
-        const elNull = document.querySelector<HTMLElement>(el);
-        if (elNull === null) {
-            throw new Error(`Could not find element with selector "${el}"`);
-        }
-        element = elNull;
-    } else {
-        element = el;
-    }
-    return new JQueryHTMLElement(element);
+export function $(el: string | HTMLTypes): JQuery {
+    return new JQuery(el);
 }
 
 export function zip<TA, TB>(a: TA[], b: TB[]) {
